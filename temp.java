8d2caec3a1c7db6c208785c068f4ee396b5b6851
Mahesh-Krishna <40786718+Mahesh-Krishna@users.noreply.github.com>
2018-08-10 23:13:44 +0530

    Add files via upload

---
+++ temp.java
@@ -0,0 +1,9 @@
+public class temp {
+    public static void main(String[] args)
+    {
+        String number = "10";
+        Integer result = Integer.valueOf(number);
+        System.out.println(result);
+
+    }
+}
